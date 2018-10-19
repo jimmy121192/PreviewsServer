0ef944d41e9e2c4d654899638179bcebd8b46627
Alone Dreamer <32500790+jimmy121192@users.noreply.github.com>
2018-10-19 12:13:40 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 var app = express();
-
+var fetch = require('node-fetch');
 const port = process.env.PORT || 3000;
 
 
@@ -14,10 +14,22 @@ app.use(function(req, res, next) {
 
 // var results = [];
 app.get("/barcode/:upc", function(req, resp){
-	var upc = req.params.upc;
+    var upc = req.params.upc;
+    
+    fetch("http://api.walmartlabs.com/v1/items?apiKey=c9tjdnbch2ehb7ta56qsv46k&upc=" + upc).then(function(resp){
+			
+        return resp.json();
+    }).then(function(json){
+        console.log(json);
+        // document.getElementById("display").innerHTML = "";
+        // for (var i =0; i<json.length; i++){
+        // 	var ndiv = document.createElement("div");
+        // 	ndiv.innerHTML = json[i];
+        // 	document.getElementById('display').appendChild(ndiv);
+        // }
+        resp.json(json);
+    });
 
-	// results.push(product);
-    resp.send(upc);
     console.log(upc)
 })
 
