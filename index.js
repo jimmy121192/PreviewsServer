ad770ec893c9f64f639afaf19e6e9fd46a8eedc6
Alone Dreamer <32500790+jimmy121192@users.noreply.github.com>
2018-10-19 11:53:03 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -5,33 +5,20 @@ const port = process.env.PORT || 3000;
 
 
 
-// Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-});
+  });
 
-var upc = '';
-var results = [];
-app.get("http://api.walmartlabs.com/v1/items?apiKey={c9tjdnbch2ehb7ta56qsv46k}&upc=035000521019", function(req, resp){
-	var product = req.params.product;
 
-	results.push(product);
-	resp.send(results);
+// var results = [];
+app.get("/barcode/: upc", function(req, resp){
+	var upc = req.params.upc;
+
+	// results.push(product);
+    resp.send(upc);
+    console.log(upc)
 })
 
 app.listen(port, function(err){
