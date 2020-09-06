86f299f2e28fccbae3e53b0a5cee65368326ce34
Jimmy Truong <hoangbao1992@gmail.com>
2020-09-06 07:56:00 -0700

    update

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const port = process.env.PORT || 3000;
 
 //Previews
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Origin", "https://impetus.netlify.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
